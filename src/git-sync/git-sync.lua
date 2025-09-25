bcfd2211a15995e582bf145a4752c52d4650210c
caleb <caleb@makyr.org>
2025-09-25 02:38:07 -0500

    Get rid of redundant tools.

--- src/git-sync/git-sync.lua
+++
@@ -1,9 +0,0 @@
--- Sync link to a public git repo.
-
-
--- Retrieve repository information.
--- Grab all current files from main branch.
--- Trash older files.
-
-
-
--- src/wget-sync/wget-sync.lua
+++
@@ -1,2 +0,0 @@
-
--- Get a full repository. 
